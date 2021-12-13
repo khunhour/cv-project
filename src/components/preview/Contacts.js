e481d0a859361456c16d9d043d33004e1ef082f8
Khunhour Morm <khonhourmorm@gmail.com>
2021-12-13 23:21:18 +0900

    imported images

--- src/components/preview/Contacts.js
+++ src/components/preview/Contacts.js
@@ -1,20 +1,23 @@
 import React from "react";
+import emailIcon from "../../images/email.png";
+import addressIcon from "../../images/address.png";
+import phoneIcon from "../../images/phone.png";
 
 export default function Contacts(props) {
 	const { email, address, tel } = props.contacts;
 	return (
-		<div id="contacts" src="../images">
+		<div id="contacts">
 			<h3 className="heading">Contacts</h3>
 			<div className="email">
-				<img src=".../images/email.png" alt="email" />
+				<img src={emailIcon} alt="email" />
 				<div>{email}</div>
 			</div>
 			<div className="tel">
-				<img src=".../images/phone.png" alt="phone" />
+				<img src={phoneIcon} alt="phone" />
 				<div>{tel}</div>
 			</div>
 			<div className="address">
-				<img src=".../images/address" alt="address" />
+				<img src={addressIcon} alt="address" />
 				<div>{address}</div>
 			</div>
 		</div>
--- src/components/preview/Preview.js
+++ src/components/preview/Preview.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "./Banner";
+import Contacts from "./Contacts";
 
 export default function Preview({ info }) {
 	return (
 		<div>
 			<Banner personalInfo={info.personalInfo} />
+			<Contacts contacts={info.contacts} />
 		</div>
 	);
 }
