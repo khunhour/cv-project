d446ffec5d2fb5553ce1662c5fc209b1446f4ad8
Khunhour Morm <khonhourmorm@gmail.com>
2021-12-16 21:12:37 +0900

    styled the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-
+import githubImg from "../images/github.png";
 export default class Footer extends Component {
 	render() {
 		return (
 			<footer>
 				<div>2021 &copy; khunhour</div>
-				<a href="#">
-					<img src="" alt="github" />
+				<a href="https://github.com/khunhour/cv-project">
+					<img src={githubImg} alt="github" />
 				</a>
 			</footer>
 		);
